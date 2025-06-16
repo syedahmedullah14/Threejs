295e53cce80d926c198c21f686613d11453db537
Jaser <syedahmedullahjaser@gmail.com>
2025-06-16 08:27:08 +0530

    Created a camera

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ import * as THREE from 'three';
 //create a scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color('#F0F0F0');
+
+//create a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
