bf9845f94fcd798d32c48a25034698408213e36a
Jaser <syedahmedullahjaser@gmail.com>
2025-06-16 08:30:57 +0530

    Created a cube

--- main.js
+++ main.js
@@ -7,3 +7,9 @@ scene.background = new THREE.Color('#F0F0F0');
 //create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
+
+//create a cude object
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshLambertMaterial({color: '#468585', emissive: '#468585'});
+
+const cube = new THREE.Mesh(geometry, material);
