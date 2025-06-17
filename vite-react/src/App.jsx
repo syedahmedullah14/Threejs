8b6fb4512cbc1a53c2b29e68fd03ff51de18993c
Jaser <syedahmedullahjaser@gmail.com>
2025-06-17 10:04:26 +0530

    Added a cylinder

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -6,8 +6,13 @@ const RotatingCube = () => {
   useFrame(() => {
     if (meshRef.current) {
       meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
     }
   })
+  return <mesh ref={meshRef}>
+    <cylinderGeometry args={[1, 1, 1]}/>
+    <meshLambertMaterial color={"#468585"} emissive={"#468585"} />
+  </mesh>
 }
 
 const App = () =>{
