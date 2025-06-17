936b520e8cab40fa463b6860d7d1805a42228305
Jaser <syedahmedullahjaser@gmail.com>
2025-06-17 09:57:27 +0530

    Added a rotating cube

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,9 +1,22 @@
 import { Canvas } from "@react-three/fiber";
 
+const RotatingCube = () => {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+    }
+  })
+}
+
 const App = () =>{
   return <Canvas style={{height: "100vh", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center"}}>
     <OrbitControls enableZoom enablePan enableRotate enableKeys/>
-    Hello World
+    
+    <directionalLight position={[1, 1, 1]} intensity={10} color={"0x9CDBA6"}/>
+    <color attach="background" args={["#F0F0F0"]}/>
+
   </Canvas>
 }
 
