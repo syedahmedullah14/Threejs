3db21aec132c8564ab3750b170c34a31e923ae68
Jaser <syedahmedullahjaser@gmail.com>
2025-06-17 10:17:03 +0530

    Added sparkles

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,4 +1,6 @@
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, Sparkles } from "@react-three/drei";
+import { useRef } from "react";
 
 const RotatingCube = () => {
   const meshRef = useRef();
@@ -9,18 +11,43 @@ const RotatingCube = () => {
       meshRef.current.rotation.y += 0.01;
     }
   })
+
   return <mesh ref={meshRef}>
     <cylinderGeometry args={[1, 1, 1]}/>
     <meshLambertMaterial color={"#468585"} emissive={"#468585"} />
   </mesh>
 }
 
+const SparklesComponent = () => {
+  return (
+    <Sparkles 
+      count={50} 
+      scale={4} 
+      size={6} 
+      speed={0.3} 
+      noise={0.5} 
+      color="red"
+      position={[0, 0, 0]}
+    />
+  )
+}
+
 const App = () =>{
   return <Canvas style={{height: "100vh", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center"}}>
     <OrbitControls enableZoom enablePan enableRotate enableKeys/>
     
-    <directionalLight position={[1, 1, 1]} intensity={10} color={"0x9CDBA6"}/>
-    <color attach="background" args={["#F0F0F0"]}/>
+    <ambientLight intensity={0.5} />
+    <directionalLight position={[1, 1, 1]} intensity={10} color={"#9CDBA6"}/>
+    <color attach="background" args={["#000000"]}/>
+
+    <RotatingCube />
+    <SparklesComponent />
+    
+    {/* Test sphere to verify scene is working */}
+    <mesh position={[3, 0, 0]}>
+      <sphereGeometry args={[0.5, 16, 16]} />
+      <meshStandardMaterial color="blue" />
+    </mesh>
 
   </Canvas>
 }
